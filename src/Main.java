354a0aac37c4a8b12d3176acf51ebdaf461099e2
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2022-10-14 14:09:38 +0200

    solve ex. 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a = 4;
+        double b = 6;
+        double c = 123;
+
+        double x = najwieksza(a, b, c);
+        System.out.println("najwieksza liczba to " + x);
+    }
+
+    private static double najwieksza(double a, double b, double c) {
+        if (a > b) {
+            return Math.max(a, c);
+        }
+        return Math.max(b, c);
     }
 }
