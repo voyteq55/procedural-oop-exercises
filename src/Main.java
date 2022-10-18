f04a772b5548c2cca3e3ff9ddd014f1f6b4771e0
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2022-10-18 14:01:50 +0200

    solve ex. 4

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("podaj liczbe n: ");
+
+        int n = scanner.nextInt();
+        System.out.println();
+
+        int licznik_ulamka = licznik(n);
+        int mianownik_ulamka = mianownik(n);
+        double wartosc = (double) licznik_ulamka / (double) mianownik_ulamka;
+        System.out.print("wartosc calego ulamka: " + wartosc);
+    }
+
+    private static int licznik(int n) {
+        int temp = 2;
+        int suma_wyrazow_ciagu = 0;
+        System.out.print("wyrazy pierwszego ciagu (" + n + "-elementowego): ");
+        for (int i = 1; i <= n; i++) {
+            suma_wyrazow_ciagu += temp;
+            System.out.print(temp + " ");
+            temp += 3;
+        }
+        System.out.println();
+        return suma_wyrazow_ciagu;
+    }
+
+    private static int mianownik(int n) {
+        int temp = 3;
+        int iloczyn_wyrazow_ciagu = 1;
+        System.out.print("wyrazy drugiego ciagu (" + n + "-elementowego): ");
+        for (int i = 1; i <= n; i++) {
+            iloczyn_wyrazow_ciagu *= temp;
+            System.out.print(temp + " ");
+            temp *= -1;
+            if (temp > 0) {
+                temp += 4;
+            } else {
+                temp -= 4;
+            }
+        }
+        System.out.println();
+        return iloczyn_wyrazow_ciagu;
+    }
+}
