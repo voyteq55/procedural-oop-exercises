320796557969867ec3b5de3659fbb3c7ad11d210
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2022-10-18 12:32:59 +0200

    added circle drawing

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         double x = najwieksza(a, b, c);
         System.out.println("najwieksza liczba to " + x);
+
+        narysuj_okrag(25);
     }
 
     private static double najwieksza(double a, double b, double c) {
@@ -13,5 +15,18 @@ public class Main {
             return Math.max(a, c);
         }
         return Math.max(b, c);
+    }
+
+    private static void narysuj_okrag(int r) {
+        for (int i = -r; i <= r; i++) {
+            for (int j = -r; j <= r; j++) {
+                if (i*i + j*j <= (r+0.1)*(r+0.1)) {
+                    System.out.print("*  ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
