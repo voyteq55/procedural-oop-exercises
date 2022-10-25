90f560e7ca0d62446b71d08d48624f0f0d8fdc5b
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2022-10-25 11:22:35 +0200

    Show result of Hermite polynomial with given input

--- src/Prog/Prog.java
+++ src/Prog/Prog.java
@@ -4,7 +4,9 @@ import Wielomian.Wielomian;
 
 public class Prog {
     public static void main(String[] args) {
-        double x = Wielomian.Hermite(2, 5);
-        System.out.println(x);
+        int n = 5;
+        double x = 2;
+        double wynik = Wielomian.Hermite(n, x);
+        System.out.println(wynik);
     }
 }
